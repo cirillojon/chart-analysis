7e035f19b97857f432a82d558cca82f8f9f34701
Jon <jonathancirillo@outlook.com>
2023-08-12 17:25:22 -0400

    Create chart.py

---
+++ chart.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+def fetch_and_plot(ticker_symbol, start_date="2020-01-01", end_date="2023-01-01"):
+    # Fetch data
+    ticker = yf.Ticker(ticker_symbol)
+    data = ticker.history(start=start_date, end=end_date)
+    
+    # Plot data
+    plt.figure(figsize=(12, 6))
+    data['Close'].plot(title=f'{ticker_symbol} Stock Closing Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Closing Price (in currency)')
+    plt.grid(True)
+    plt.show()
+
+# Example Usage
+ticker = input("Enter the stock ticker: ")
+fetch_and_plot(ticker)
