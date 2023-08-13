aa069c8c17c20e9d359294e3227516db11b7647f
Jon <jonathancirillo@outlook.com>
2023-08-12 20:30:37 -0400

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,44 +7,72 @@ from sklearn.preprocessing import MinMaxScaler
 from keras import models
 from keras import layers
 
-# Fetch stock data
-ticker_symbol = "AAPL"
-data = yf.download(ticker_symbol, start="2010-01-01", end="2023-01-01")['Adj Close']
+def fetch_and_predict(ticker_symbol):
+    # Fetch stock data
+    data = yf.download(ticker_symbol, start="2010-01-01", end="2023-01-01")['Adj Close']
 
-# Normalize data
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
+    # Normalize data
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
 
-# Create training dataset with 60 time-steps for each sample
-X, y = [], []
-for i in range(60, len(scaled_data)):
-    X.append(scaled_data[i-60:i])
-    y.append(scaled_data[i])
-X, y = np.array(X), np.array(y)
+    # Create training dataset with 60 time-steps for each sample
+    X, y = [], []
+    for i in range(60, len(scaled_data)):
+        X.append(scaled_data[i-60:i])
+        y.append(scaled_data[i])
+    X, y = np.array(X), np.array(y)
 
-# Split into training and test sets
-train_size = int(0.8 * len(X))
-X_train, y_train = X[:train_size], y[:train_size]
-X_test, y_test = X[train_size:], y[train_size:]
+    # Split into training and test sets
+    train_size = int(0.8 * len(X))
+    X_train, y_train = X[:train_size], y[:train_size]
+    X_test, y_test = X[train_size:], y[train_size:]
 
-# Build LSTM model
-model = models.Sequential()
-model.add(layers.LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
-model.add(layers.LSTM(units=50))
-model.add(layers.Dense(units=1))
+    # Build LSTM model
+    model = models.Sequential()
+    model.add(layers.LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(layers.LSTM(units=50))
+    model.add(layers.Dense(units=1))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, epochs=3, batch_size=32)
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(X_train, y_train, epochs=3, batch_size=32)
 
-# Predict using the test set
-predicted_prices = model.predict(X_test)
-predicted_prices = scaler.inverse_transform(predicted_prices)
+    # Predict using the test set
+    predicted_prices = model.predict(X_test)
+    predicted_prices = scaler.inverse_transform(predicted_prices)
 
-# Visualization
-plt.figure(figsize=(14,6))
-plt.plot(data.index[60:train_size], data[60:train_size], color='blue', label="Training data")
-plt.plot(data.index[train_size+60:], data[train_size+60:], color='green', label="Actual Price")
-plt.plot(data.index[train_size+60:], predicted_prices, color='red', label="Predicted Price")
-plt.title(f"{ticker_symbol} Stock Price Prediction using LSTM")
-plt.legend()
-plt.show()
+    return data[train_size+60:], predicted_prices, data.index[60:train_size], data[60:train_size]
+
+def show_plot(train_indexes, training_data, actual, predicted):
+    plt.figure(figsize=(14, 6))
+    plt.plot(train_indexes, training_data, color='blue', label="Training data")
+    plt.plot(actual.index, actual.values, color='green', label="Actual Price")
+    plt.plot(actual.index, predicted, color='red', label="Predicted Price")
+    plt.legend()
+    plt.show()
+
+def on_submit():
+    ticker_symbol = ticker_input.get()
+    if not ticker_symbol:
+        messagebox.showerror("Error", "Please enter a valid ticker symbol.")
+        return
+
+    try:
+        actual, predicted, train_indexes, training_data = fetch_and_predict(ticker_symbol)
+        show_plot(train_indexes, training_data, actual, predicted)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+app = tk.Tk()
+app.title("Stock Price Prediction")
+
+frame = tk.Frame(app)
+frame.pack(padx=10, pady=10)
+
+tk.Label(frame, text="Enter Stock Ticker:").grid(row=0, column=0, padx=5, pady=5)
+ticker_input = tk.Entry(frame)
+ticker_input.grid(row=0, column=1, padx=5, pady=5)
+
+submit_btn = tk.Button(frame, text="Predict & Plot", command=on_submit)
+submit_btn.grid(row=1, columnspan=2, pady=10)
+
+app.mainloop()
